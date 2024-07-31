816d1b7c61730624e2f9e556119ff41b934e197f
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2024-08-01 04:58:30 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-red-500'>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque ex corporis illo deleniti adipisci, voluptates voluptatibus quibusdam dolore non iusto reiciendis cum saepe delectus tempora et. Deserunt magni natus non?
+    </div>
+  )
+}
+
+export default App

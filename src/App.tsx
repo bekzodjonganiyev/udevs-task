4c395f658055b45bd96b321d69e151b69edd4b9b
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2024-08-01 05:07:17 +0500

    change view commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <div className='text-red-500'>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque ex corporis illo deleniti adipisci, voluptates voluptatibus quibusdam dolore non iusto reiciendis cum saepe delectus tempora et. Deserunt magni natus non?
+      Lorem.
     </div>
   )
 }
